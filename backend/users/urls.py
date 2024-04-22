4b9cf407d674351605ed50417c7862778f05064c
tibarekb <berassut@gmail.com>
2024-04-22 17:22:01 +0300

    login page configuration

---
+++ backend/users/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin 
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from users.views import CreateUserView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("users/user/register", CreateUserView.as_view(), name="register"),
+    path("users/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("users/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("users-auth/", include("rest_framework.urls")),
+]
